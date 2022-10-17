2091ffe44e8c76a7bf3d02b9c5ada0a62abc7618
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-10-17 16:38:30 -0400

    Assignment 2: Add Contact router and controller for CREATE operation

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -21,6 +21,36 @@ module.exports.contactIndex = function (req, res, next) {
   }).sort({ firstName: 1 });
 };
 
+module.exports.displayAddPage = (req, res, next) => {
+  const newContact = Contact();
+
+  res.render("contacts/add_edit", {
+    title: "Add New Contact",
+    contact: newContact,
+  });
+};
+
+module.exports.processAddPage = (req, res, next) => {
+  const newContact = Contact({
+    _id: req.body.id,
+    firstName: req.body.first_name,
+    lastName: req.body.last_name,
+    phoneNumber: req.body.phone_number,
+    email: req.body.email,
+  });
+
+  Contact.create(newContact, (err, contact) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    } else {
+      // refresh the contact list
+      console.log(contact);
+      res.redirect("/contacts");
+    }
+  });
+};
+
 module.exports.delete = (req, res, next) => {
   const id = req.params.id;
 
--- routes/contacts.router.js
+++ routes/contacts.router.js
@@ -12,6 +12,12 @@ let contactsController = require("../controllers/contacts.controller");
 /* GET contacts listing. */
 router.get("/", contactsController.contactIndex);
 
+/* GET route for displaying the Add page - CREATE operation */
+router.get('/add', contactsController.displayAddPage);
+
+/* POST route for processing the Add page - CREATE operation */
+router.post("/add", contactsController.processAddPage);
+
 /* DELETE contact. */
 router.get("/delete/:id", contactsController.delete);
 
