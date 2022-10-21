301623539a7f97cc164885317ca41c44929acd73
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-10-21 09:36:47 -0400

    Assignment 2: Modify format of error message

--- helpers/getErrorMessage.js
+++ helpers/getErrorMessage.js
@@ -21,7 +21,7 @@ const getErrorMessage = (err) => {
   } else {
     for (let errName in err.errors) {
       if (err.errors[errName].message) {
-        message += err.errors[errName].message;
+        message += `${err.errors[errName].message}\n`;
       }
     }
   }
